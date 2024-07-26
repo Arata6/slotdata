2bf3a1418232228df64ab8af1f52fef5c23b1630
Arata6 <24W604D@shinshu-u.ac.jp>
2024-07-27 02:43:48 +0900

    Procfile for Heroku

---
+++ app/Models/Parlors.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Parlors extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2024_07_26_171643_create_data_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data', function (Blueprint $table) {
+            $table->id();
+            $table->string('date', 50);
+            $table->string('event', 50);
+            $table->string('participants', 50);
+            $table->string('total', 50);
+            $table->string('average', 50);
+            $table->string('body', 50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data');
+    }
+};
